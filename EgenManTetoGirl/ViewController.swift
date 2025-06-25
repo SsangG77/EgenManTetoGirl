a2ae3e4c7afe1d43519d8f9a43f30540377fc45c
sangjin <sangjincha719@gmail.com>
2025-06-26 04:38:41 +0900

    Initial Commit

---
+++ EgenManTetoGirl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EgenManTetoGirl
+//
+//  Created by 차상진 on 6/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EgenManTetoGirlTests/EgenManTetoGirlTests.swift
@@ -0,0 +1,17 @@
+//
+//  EgenManTetoGirlTests.swift
+//  EgenManTetoGirlTests
+//
+//  Created by 차상진 on 6/26/25.
+//
+
+import Testing
+@testable import EgenManTetoGirl
+
+struct EgenManTetoGirlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
